0368eb088cfb0490289c5c97f186d2d27050725f
Marquez77 <62211625+Marquez77@users.noreply.github.com>
2022-09-14 22:44:17 +0900

    Tested to apply color of glowing

--- src/main/java/me/marquez/cgapi/CustomGlowAPI.java
+++ src/main/java/me/marquez/cgapi/CustomGlowAPI.java
@@ -6,20 +6,28 @@ import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.wrappers.Pair;
 import com.comphenix.protocol.wrappers.WrappedDataWatcher;
-import org.bukkit.Color;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundSetPlayerTeamPacket;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.scores.PlayerTeam;
+import net.minecraft.world.scores.Scoreboard;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CustomGlowAPI {
 
     private CustomGlowAPI() {}
 
     private static final ProtocolManager manager = ProtocolLibrary.getProtocolManager();
-    private static final Map<Pair<Integer, Player>, Color> glowingData = new HashMap<>();
+    private static final Map<Pair<Integer, Player>, ChatColor> glowingData = new HashMap<>();
 
     public static boolean isGlowing(Entity entity, Player receiver) {
         return isGlowing(entity.getEntityId(), receiver);
@@ -31,7 +39,7 @@ public class CustomGlowAPI {
 
     public static void setGlowing(Entity entity, Player receiver) {
         int entityId = entity.getEntityId();
-        glowingData.put(new Pair<>(entityId, receiver), Color.WHITE);
+        glowingData.put(new Pair<>(entityId, receiver), ChatColor.WHITE);
         PacketContainer packet = manager.createPacket(PacketType.Play.Server.ENTITY_METADATA);
         packet.getIntegers().write(0, entityId);
         applyGlowing(packet, entity);
@@ -49,4 +57,38 @@ public class CustomGlowAPI {
         dataWatcher.setObject(0, serializer, (byte)0x40);
         packet.getWatchableCollectionModifier().write(0, dataWatcher.getWatchableObjects());
     }
+
+
+    //SCOREBOARD
+    private static Map<ChatColor, PlayerTeam> teams = new HashMap<>();
+    private static Set<Pair<Player, ChatColor>> knownChatColors = new HashSet<>();
+
+    protected static void clearColors(Player player) {
+        knownChatColors.remove(player);
+    }
+
+    private static void applyTeam(Entity entity, ChatColor color, Player receiver) {
+        PlayerTeam team = teams.compute(color, (key, value) -> {
+            Scoreboard scoreboard = new Scoreboard();
+            PlayerTeam playerTeam = new PlayerTeam(scoreboard, "CGAPI#" + color.name());
+            playerTeam.setPlayerPrefix(Component.literal(color.toString()));
+            playerTeam.setPlayerSuffix(Component.literal(""));
+            playerTeam.setColor(ChatFormatting.getByName(color.name()));
+            return playerTeam;
+        });
+        Pair<Player, ChatColor> key = new Pair<>(receiver, color);
+        if(!knownChatColors.contains(key)) {
+            knownChatColors.add(key);
+            ClientboundSetPlayerTeamPacket createTeamPacket = ClientboundSetPlayerTeamPacket.createAddOrModifyPacket(team, true);
+            sendPackets(receiver, createTeamPacket);
+        }
+        ClientboundSetPlayerTeamPacket addPacket = ClientboundSetPlayerTeamPacket.createPlayerPacket(team, entity instanceof OfflinePlayer ? entity.getName() : entity.getUniqueId().toString(), ClientboundSetPlayerTeamPacket.Action.ADD);
+        sendPackets(receiver, addPacket);
+    }
+
+    private static void sendPackets(Player player, Packet<?>... packets) {
+        ServerGamePacketListenerImpl connection = ((CraftPlayer)player).getHandle().connection;
+        Arrays.stream(packets).forEach(connection::send);
+    }
+
 }
--- src/main/java/me/marquez/cgapi/CustomGlowPlugin.java
+++ src/main/java/me/marquez/cgapi/CustomGlowPlugin.java
@@ -6,9 +6,16 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.WrappedWatchableObject;
+import net.minecraft.network.protocol.game.ClientboundSetPlayerTeamPacket;
+import net.minecraft.world.scores.PlayerTeam;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CustomGlowPlugin extends JavaPlugin {
 
@@ -28,12 +35,41 @@ public class CustomGlowPlugin extends JavaPlugin {
                     int entityId = container.getIntegers().read(0);
                     if (CustomGlowAPI.isGlowing(entityId, event.getPlayer())) {
                         List<WrappedWatchableObject> value = container.getWatchableCollectionModifier().read(0);
-                        value.stream().filter(v -> v.getIndex() == 0).forEach(v -> v.setValue((byte) ((int) v.getValue() | 0x40)));
+                        value.stream().filter(v -> v.getIndex() == 0).forEach(v -> v.setValue((byte) ((byte) v.getValue() | 0x40)));
                         container.getWatchableCollectionModifier().write(0, value);
                     }
                 }
             }
         );
+        ProtocolLibrary.getProtocolManager().addPacketListener(
+                new PacketAdapter(this, PacketType.Play.Server.SCOREBOARD_TEAM) {
+                    @Override
+                    public void onPacketSending(PacketEvent event) {
+                        PacketContainer packet = event.getPacket();
+                        Optional optional = (Optional)packet.getModifier().read(3);
+                        optional.ifPresent(o -> {
+                            ClientboundSetPlayerTeamPacket.Parameters parameters = (ClientboundSetPlayerTeamPacket.Parameters) o;
+                            if(!parameters.getDisplayName().toString().startsWith("literal{CIT-")) {
+                                System.out.println("display: " + parameters.getDisplayName());
+                                System.out.println("prefix: " + parameters.getPlayerPrefix());
+                                System.out.println("suffix: " + parameters.getPlayerSuffix());
+                                System.out.println("color: " + parameters.getColor());
+                                System.out.println("options: " + parameters.getOptions());
+                            }
+                        });
+
+//                        ClientboundSetPlayerTeamPacket.createPlayerPacket()
+//                        PlayerTeam
+                    }
+                }
+        );
+        getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler
+            public void onQuit(PlayerQuitEvent e) {
+                CustomGlowAPI.clearColors(e.getPlayer());
+            }
+
+        }, this);
         getLogger().info("Enabled CustomGlowPlugin.");
     }
 
